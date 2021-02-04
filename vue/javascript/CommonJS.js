596ac67a7dd43c058c1bb4a0382a5fed37abcfd7
lorreta-lab <77911556+lorreta-lab@users.noreply.github.com>
2021-02-04 17:11:51 +0800

    Add files via upload

---
+++ vue/javascript/CommonJS.js
@@ -0,0 +1,8 @@
+
+        module.exports={
+            a:1
+        }
+        console.log(module.exports.a)
+        var module=require('./a')
+        console.log(module.a)
+        console.log(module.b)
---
+++ vue/javascript/a.js
@@ -0,0 +1,9 @@
+module.exports={
+    a:1,
+    b:2
+}
+var exports=module.exports// can't loss it
+exports.b=3
+
+
+
